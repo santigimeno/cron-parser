66968d7811ffc98785c0364a1394834da6af9040
Santiago Gimeno <santiago.gimeno@ionide.es>
2016-02-26 00:53:50 +0100

    In DST end

--- test/timezone.js
+++ test/timezone.js
@@ -53,25 +53,54 @@ test('It works on DST start', function(t) {
   t.end();
 });
 
-// test('It works on DST end', function(t) {
-//   try {
-//     var options = {
-//       currentDate: '2016-10-30 02:00:01',
-//       tz: 'Europe/Athens'
-//     };
+test('It works on DST end', function(t) {
+  try {
+    var options = {
+      currentDate: '2016-10-30 02:00:01',
+      tz: 'Europe/Athens'
+    };
 
-//     var interval = CronExpression.parse('0 * * * *', options);
-//     t.ok(interval, 'Interval parsed');
+    var interval = CronExpression.parse('0 * * * *', options);
+    t.ok(interval, 'Interval parsed');
 
-//     var date = interval.next();
-//     t.equal(date.getHours(), 3, '3 AM');
-//     date = interval.next();
-//     t.equal(date.getHours(), 3, 'Due to DST end in Athens (4-->3)');
-//     date = interval.next();
-//     t.equal(date.getHours(), 4, '4 AM');
-//   } catch (err) {
-//     t.ifError(err, 'Interval parse error');
-//   }
+    var date = interval.next();
+    t.equal(date.getHours(), 3, '3 AM');
+    date = interval.next();
+    t.equal(date.getHours(), 3, 'Due to DST end in Athens (4-->3)');
+    date = interval.next();
+    t.equal(date.getHours(), 4, '4 AM');
 
-//   t.end();
-// });
+    var interval = CronExpression.parse('0 3 * * *', options);
+    t.ok(interval, 'Interval parsed');
+
+    var date = interval.next();
+    t.equal(date.getHours(), 3, '3 AM');
+    date = interval.next();
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 31, '31st');
+
+    var interval = CronExpression.parse('*/20 3 * * *', options);
+    t.ok(interval, 'Interval parsed');
+
+    var date = interval.next();
+    t.equal(date.getMinutes(), 0, '0');
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 30, '30th');
+    date = interval.next();
+    t.equal(date.getMinutes(), 20, '20');
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 30, '30th');
+    date = interval.next();
+    t.equal(date.getMinutes(), 40, '40');
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 30, '30th');
+    date = interval.next();
+    t.equal(date.getMinutes(), 0, '0');
+    t.equal(date.getHours(), 3, '3 AM');
+    t.equal(date.getDate(), 31, '31st');
+   } catch (err) {
+    t.ifError(err, 'Interval parse error');
+  }
+
+  t.end();
+});
